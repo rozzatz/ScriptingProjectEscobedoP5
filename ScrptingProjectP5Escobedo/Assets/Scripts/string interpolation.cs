cef7370a1b65aed9af48d681240ecd81902bcf45
bruce <753825@student.dusd.net>
2024-02-14 13:49:17 -0800

    zdcfgwer

--- ScrptingProjectP5Escobedo/Assets/Scripts/string interpolation.cs
+++ ScrptingProjectP5Escobedo/Assets/Scripts/string interpolation.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class stringinterpolation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /*
+    // In this case, result = 4
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+
+    Vector3 from = new Vector3(1f, 2f, 3f);
+    Vector3 to = new Vector3(5f, 6f, 7f);
+
+    // Here result = (4, 5, 6)
+    Vector3 result = Vector3.Lerp(from, to, 0.75f);
+
     void Start()
     {
         
@@ -13,6 +22,10 @@ public class stringinterpolation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+            light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+    //here the code is saying if thr intensirty of the light starts at 0 then after the first update it will be set to 4 the next frame it will be set to 6 then to 7 then to 7.5 thus over deveral frames the ligghts intensity will tend towards 8
+     light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+//here instead of the chnage happening over frames it will be over seconds                                                                  
     }
+    */
 }
